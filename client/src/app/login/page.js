2f842733f972cfe636f9a78fd775942e404b2553
Esi Yankey <graceyan244@gmail.com>
2023-11-09 20:03:12 +0000

    fixed layout bug

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -12,14 +12,14 @@ import {
 export default function login() {
   return (
     
-      <div className=" text-center ">
-        <h1 className="text-">Welcome</h1>
-        <h3>Welcome! Log in to continue your journey with us.</h3>
+      <div className="bg-white flex flex-col justify-center items-center text-center ">
+        <h1 className="text-7xl border">Welcome</h1>
+        <h3 className="text-base">Welcome! Log in to continue your journey with us.</h3>
 
         <form>
           <div className="flex">
             <BiUser />
-            <input type="text"  placeholder="username" />
+            <input type="text" className="py-9 px-3 " placeholder="username" />
           </div>
           <div className="flex ">
             <AiOutlineLock />
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -34,58 +34,59 @@ export const Navbar = () => {
           scroll ? "active" : ""
         }`}
       >
-       
-          <div className="flex leading-none relative right-4 sm-right-0 ">
+        <div className="flex leading-none relative right-4 sm-right-0 ">
+          <div className="w-28">
             <Image
               src="/images/a-round-gradient.png"
               alt="A.round logo"
-              width={100}
-              height={10}
-              layout="fixed"
+              fill={true}
             />
+          </div>
+          <div className="w-28">
             <Image
               src="/images/a-round.png"
               alt="A.round logo"
-              className="relative right-7 "
-              width={100}
-              height={10}
-              layout="fixed"
+              className="relative right-7  "
+              fill={true}
             />
           </div>
-          <div className=" leading-none items-center   md:flex">
-            <div className={`left-[10rem] flex gap-7 font-[500] pl-7  `}>
-              <ul className="md:flex  md:items-center md:justify-center cursor-pointer md:gap-9 text-[0.9rem] md:text-[0.84rem] text-[#ced8e3]">
-                <li className="hover:text-white duration-700 py-3 md:py-0">
-                  Subscription
-                </li>
-                <li className="hover:text-white  duration-700 py-3  md:py-0">
-                  Courses
-                </li>
-                <li className="hover:text-white duration-700   md:py-0 py-3">
-                  Learning Paths
-                </li>
-                <li className="hover:text-white duration-700  md:py-0 py-3">
-                  Forum
-                </li>
-                <Link
-                  href="/contact"
-                  className="hover:text-white duration-700  md:py-0 py-3"
-                >
-                  Contact
-                </Link>
-              </ul>
-              <Link href="/login" className=" px-2 py-1 md:px-3 md:py-3 text-[#ffffff] bg-[#8244ff]  text-[0.7rem] md:text-[1rem] relative right-4 sm:right-0  rounded-[30px] hover:bg-[#6d32e4] duration-700">
-                Member Area
+        </div>
+        <div className=" leading-none items-center   md:flex">
+          <div className={`left-[10rem] flex gap-7 font-[500] pl-7  `}>
+            <ul className="md:flex  md:items-center md:justify-center cursor-pointer md:gap-9 text-[0.9rem] md:text-[0.84rem] text-[#ced8e3]">
+              <li className="hover:text-white duration-700 py-3 md:py-0">
+                Subscription
+              </li>
+              <li className="hover:text-white  duration-700 py-3  md:py-0">
+                Courses
+              </li>
+              <li className="hover:text-white duration-700   md:py-0 py-3">
+                Learning Paths
+              </li>
+              <li className="hover:text-white duration-700  md:py-0 py-3">
+                Forum
+              </li>
+              <Link
+                href="/contact"
+                className="hover:text-white duration-700  md:py-0 py-3"
+              >
+                Contact
               </Link>
-            </div>
+            </ul>
+            <Link
+              href="/login"
+              className=" px-2 py-1 md:px-3 md:py-3 text-[#ffffff] bg-[#8244ff]  text-[0.7rem] md:text-[1rem] relative right-4 sm:right-0  rounded-[30px] hover:bg-[#6d32e4] duration-700"
+            >
+              Member Area
+            </Link>
           </div>
-          <button className="p-2  mr-2 md:mr-6 rounded">
-            <FaBars
-              className="text-[#4e536a] text-[1.4rem] md:hidden "
-              onClick={handleShowMenu}
-            />
-          </button>
-       
+        </div>
+        <button className="p-2  mr-2 md:mr-6 rounded">
+          <FaBars
+            className="text-[#4e536a] text-[1.4rem] md:hidden "
+            onClick={handleShowMenu}
+          />
+        </button>
       </div>
       {/* 
             "use client";
